8bb1d169a5233c52edcfb8501e6753b4b46fc284
toandovn <toan.do5787@gmail.com>
2016-07-22 11:24:52 -0700

    Signed-off-by: toandovn <toan.do5787@gmail.com>

---
+++ Project/Project 2/Project2_PokerGame/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Toan
+ *
+ * Created on July 22, 2016, 11:18 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
